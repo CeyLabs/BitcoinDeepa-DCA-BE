ee7335311a34e61b9e53e0d632d63daabdd4d488
Dilshan Madushanka <helloscoopz@gmail.com>
2025-07-22 19:34:36 +0530

    fix: remove database ping

--- src/modules/knex/index.ts
+++ src/modules/knex/index.ts
@@ -2,4 +2,3 @@ export { KnexService } from './knex.service';
 export { KnexModule } from './knex.module';
 export { DatabaseLoggerService } from './database-logger.service';
 export { DatabaseSystemLogger } from './database-system-logger.service';
-export { KnexConnectionManager } from './knex-connection-manager';
--- src/modules/knex/knex.module.ts
+++ src/modules/knex/knex.module.ts
@@ -1,6 +1,5 @@
 import { Global, Module } from '@nestjs/common';
 import { KnexService } from './knex.service';
-import { KnexConnectionManager } from './knex-connection-manager';
 import { DatabaseLoggerService } from './database-logger.service';
 import { DatabaseSystemLogger } from './database-system-logger.service';
 import { DatabaseLoggerInitializer } from './database-logger-initializer.service';
@@ -9,7 +8,6 @@ import { DatabaseLoggerInitializer } from './database-logger-initializer.service
 @Module({
   providers: [
     KnexService,
-    KnexConnectionManager,
     DatabaseLoggerService,
     DatabaseSystemLogger,
     DatabaseLoggerInitializer,
