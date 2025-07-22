d7bba75f103d08310e0c92be2f220da856bff14f
Dilshan Madushanka <helloscoopz@gmail.com>
2025-07-22 19:08:58 +0530

    ci: use node version 22

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DatabaseSystemLogger } from './modules/knex/database-system-logger.service';
+import { randomUUID } from 'crypto';
+
+// Polyfill for crypto.randomUUID if not available (Node.js < 14.17.0)
+if (typeof global.crypto === 'undefined') {
+  global.crypto = {} as any;
+}
+if (typeof global.crypto.randomUUID === 'undefined') {
+  global.crypto.randomUUID = randomUUID;
+}
 
 async function bootstrap() {
   // Create the app with custom logger
